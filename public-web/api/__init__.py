2bcb755ad2d89b7307e4814cb79d1c9f05def11a
zhixian <zhixian@hotmail.com>
2020-06-15 23:15:41 +0800

    sample api

--- public-web/api/__init__.py
+++ public-web/api/__init__.py
@@ -2,6 +2,6 @@
 # Define package composition
 ################################################################################
 
-#__all__ = ["visitor"]
+__all__ = ["root", "test"]
 
 # ZX: There's a better way to do this; see other source code from elsewhere
